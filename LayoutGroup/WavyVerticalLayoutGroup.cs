4a8a8c4037e7264d4419e75535af7b00f195a2e8
nader <naderitegames@gmail.com>
2025-10-15 03:00:06 +0330

    Cleanup + Try ptimizeing and add new features

--- LayoutGroup/WavyVerticalLayoutGroup.cs
+++ LayoutGroup/WavyVerticalLayoutGroup.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace NaderiteCustomScripts
@@ -6,12 +7,19 @@ namespace NaderiteCustomScripts
     [ExecuteInEditMode]
     public class WavyVerticalLayoutGroup : VerticalLayoutGroup
     {
+        [SerializeField] private bool useWave;
+        public bool UseWave => useWave;
         [SerializeField] private AnimationCurve waveCurve = AnimationCurve.Linear(0, 0, 1, 1);
-        [SerializeField, Min(1)] private float waveLoop;
+        [SerializeField, Min(1)] private float loopCount;
         [SerializeField] private bool applyWaveToAll;
+        public bool ApplyWaveToAll => applyWaveToAll;
         [SerializeField] private int targetDistance;
         [SerializeField] private float intensity = 1;
-        [SerializeField] private Vector2 offset;
+
+        [FormerlySerializedAs("offset"), SerializeField]
+        private Vector2 positionOffset = Vector2.zero;
+
+        //[SerializeField, Range(0, 1f)] private float waveOffset;
         [SerializeField] private bool isMirror;
         public bool IsSnap => snap;
         [SerializeField] private bool snap;
@@ -66,32 +74,64 @@ namespace NaderiteCustomScripts
         {
             if (rectChildren.Count == 0) return;
             PrepareForWave();
-            foreach (var child in rectChildren)
-            {
-                if (child == null) continue;
-                var distance = (child.position - _center).y;
-                float curveTime;
-                float distanceLerpFactor = Mathf.Abs(distance / targetDistance);
-                if (isMirror)
-                    curveTime = Mathf.Lerp(0, 1, distanceLerpFactor);
-                else
-                {
-                    curveTime = distance >= 0
-                        ? Mathf.Lerp(0.5f, 1, distanceLerpFactor)
-                        : Mathf.Lerp(0.5f, 0f, distanceLerpFactor);
-                }
 
-                var curveAmount = waveCurve.Evaluate(curveTime) * (intensity * 10);
-                var newPos = child.position;
-                newPos.x += curveAmount + offset.x;
-                newPos.y += offset.y;
-                child.position = newPos;
-                if (snap && Mathf.Abs(distance) < Mathf.Abs(_lastNearDistance))
+            if (applyWaveToAll)
+            {
+                var loopArea = rectChildren.Count / loopCount;
+                for (var index = 0; index < rectChildren.Count; index++)
                 {
-                    _lastNearDistance = distance;
-                    _nearestObject = child;
+                    var child = rectChildren[index];
+                    if (child == null) continue;
+                    
+                    float curveTime;
+                    if (isMirror)
+                    {
+                        curveTime = Mathf.Lerp(-1, 1, index / loopArea % 1);
+                        curveTime = Mathf.Abs(curveTime);
+                    }
+                    else
+                    {
+                        curveTime = Mathf.Lerp(0, 1, index / loopArea % 1);
+                    }
+
+                    UpdatePositionFor(child, curveTime);
                 }
             }
+            else
+            {
+                foreach (var child in rectChildren)
+                {
+                    if (child == null) continue;
+                    
+                    var distance = (child.position - _center).y;
+                    float curveTime;
+                    float distanceLerpFactor = Mathf.Abs(distance / targetDistance);
+                    if (isMirror)
+                        curveTime = Mathf.Lerp(0, 1, distanceLerpFactor);
+                    else
+                    {
+                        curveTime = distance >= 0
+                            ? Mathf.Lerp(0.5f, 1, distanceLerpFactor)
+                            : Mathf.Lerp(0.5f, 0f, distanceLerpFactor);
+                    }
+
+                    UpdatePositionFor(child, curveTime);
+                    if (snap && Mathf.Abs(distance) < Mathf.Abs(_lastNearDistance))
+                    {
+                        _lastNearDistance = distance;
+                        _nearestObject = child;
+                    }
+                }
+            }
+        }
+        
+        void UpdatePositionFor(Transform child, float curveTime)
+        {
+            var curveAmount = waveCurve.Evaluate(curveTime) * (intensity * 10);
+            var newPos = child.position;
+            newPos.x += curveAmount + positionOffset.x;
+            newPos.y += positionOffset.y;
+            child.position = newPos;
         }
 
         void PrepareForWave()
@@ -111,7 +151,7 @@ namespace NaderiteCustomScripts
             if (Mathf.Abs(snapSettings.scrollRect.velocity.y) > snapSettings.snapSensitivity) return;
             var nearestDistance1 = Vector2.Distance(_nearestObject.position, _center);
             if (nearestDistance1 <= 0.1f) return;
-            var speed = (Time.deltaTime * Mathf.Lerp(0, 1, nearestDistance1 / snapSettings.distanceForSnapping)) *
+            var speed = Time.deltaTime * Mathf.Lerp(0, 1, nearestDistance1 / snapSettings.distanceForSnapping) *
                         (snapSettings.snappingSpeed * .1f);
             if (_center.y > _nearestObject.position.y)
                 snapSettings.scrollRect.normalizedPosition += Vector2.down * speed;
--- LayoutGroup/WavyVerticalLayoutGroupEditor.cs
+++ LayoutGroup/WavyVerticalLayoutGroupEditor.cs
@@ -7,10 +7,16 @@ namespace NaderiteCustomScripts
     [CustomEditor(typeof(WavyVerticalLayoutGroup))]
     public class WavyVerticalLayoutGroupEditor : HorizontalOrVerticalLayoutGroupEditor
     {
+        private SerializedProperty _useWave;
         private SerializedProperty _waveCurve;
+        private SerializedProperty _loopCount;
+        private SerializedProperty _applyWaveToAll;
         private SerializedProperty _targetDistance;
         private SerializedProperty _intensity;
-        private SerializedProperty _offset;
+
+        private SerializedProperty _positionOffset;
+
+        //private SerializedProperty _waveOffset;
         private SerializedProperty _isMirror;
         private SerializedProperty _snap;
         private SerializedProperty _snapSettings;
@@ -19,10 +25,14 @@ namespace NaderiteCustomScripts
         protected override void OnEnable()
         {
             base.OnEnable();
+            _useWave = serializedObject.FindProperty("useWave");
             _waveCurve = serializedObject.FindProperty("waveCurve");
+            _loopCount = serializedObject.FindProperty("loopCount");
+            _applyWaveToAll = serializedObject.FindProperty("applyWaveToAll");
             _targetDistance = serializedObject.FindProperty("targetDistance");
             _intensity = serializedObject.FindProperty("intensity");
-            _offset = serializedObject.FindProperty("offset");
+            _positionOffset = serializedObject.FindProperty("positionOffset");
+            //_waveOffset = serializedObject.FindProperty("waveOffset");
             _isMirror = serializedObject.FindProperty("isMirror");
             _snap = serializedObject.FindProperty("snap");
             _snapSettings = serializedObject.FindProperty("snapSettings");
@@ -37,16 +47,27 @@ namespace NaderiteCustomScripts
             EditorGUILayout.LabelField("Wave Effect", EditorStyles.boldLabel);
 
             serializedObject.Update();
-            EditorGUILayout.PropertyField(_waveCurve);
-            EditorGUILayout.PropertyField(_targetDistance);
-            EditorGUILayout.PropertyField(_intensity);
-            EditorGUILayout.PropertyField(_offset);
-            EditorGUILayout.PropertyField(_isMirror);
-            EditorGUILayout.PropertyField(_snap);
-            if (_target.IsSnap)
+            EditorGUILayout.PropertyField(_useWave);
+            if (_target.UseWave)
             {
-                EditorGUILayout.PropertyField(_snapSettings);
+                EditorGUILayout.PropertyField(_waveCurve);
+                EditorGUILayout.PropertyField(_intensity);
+                EditorGUILayout.PropertyField(_positionOffset);
+                //EditorGUILayout.PropertyField(_waveOffset);
+                EditorGUILayout.PropertyField(_isMirror);
+                EditorGUILayout.Space();
+                EditorGUILayout.PropertyField(_applyWaveToAll);
+                if (!_target.ApplyWaveToAll)
+                    EditorGUILayout.PropertyField(_targetDistance);
+                EditorGUILayout.PropertyField(_loopCount);
+                EditorGUILayout.Space();
+                EditorGUILayout.PropertyField(_snap);
+                if (_target.IsSnap)
+                {
+                    EditorGUILayout.PropertyField(_snapSettings);
+                }
             }
+
 
             serializedObject.ApplyModifiedProperties();
         }
